199b37e08c472d95ed5b17c1c3a0bdf8ddeaf47c
benstew <benstewart2008@gmail.com>
2015-11-20 12:54:20 -0500

    Done

--- app/router.js
+++ app/router.js
@@ -3,8 +3,13 @@ import config from './config/environment';
 
 var Router = Ember.Router.extend({  
   location: config.locationType
+
 });
 
-export default Router.map(function() {  
- 
+export default Router.map(function() {
+  this.route('hello');
+  this.route('about');
+  this.route('contributors', function(){
+    this.route('sophie');
+  });
 });
